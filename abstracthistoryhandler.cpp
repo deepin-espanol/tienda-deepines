71a79dfa26847e677a90209af54885fc52f6adcf
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2021-04-16 18:58:27 -0930

    Remove the useless

--- abstracthistoryhandler.cpp
+++
@@ -1,6 +0,0 @@
-#include "abstracthistoryhandler.h"
-
-AbstractHistoryHandler::AbstractHistoryHandler() {}
-AbstractHistoryHandler::~AbstractHistoryHandler() {}
-void AbstractHistoryHandler::load(QString) {}
-QWidget *AbstractHistoryHandler::widget() const {return new QWidget;}
